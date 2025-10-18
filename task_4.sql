688d39dc86f0d86fcfc1abc75a07b2eccfc575e4
Emmanuel Fiifi Bentsi Essuman <deeptechhub5@gmail.com>
2025-10-18 14:02:37 +0000

    fix: corrected table name

--- task_4.sql
+++ task_4.sql
@@ -1,8 +1,8 @@
 -- Use the alx_book_store database
 USE alx_book_store;
 
--- Show full description of the books table using INFORMATION_SCHEMA
+-- Show full description of the Books table using INFORMATION_SCHEMA
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'alx_book_store'
-AND TABLE_NAME = 'books';
+AND TABLE_NAME = 'Books';
