2549d06b0681dffa6250b14f6b2bd4004bcb1642
Emmanuel Fiifi Bentsi Essuman <deeptechhub5@gmail.com>
2025-10-18 13:59:36 +0000

    feat: changes made

--- task_4.sql
+++ task_4.sql
@@ -1,8 +1,8 @@
--- Select the alx_book_store database
+-- Use the alx_book_store database
 USE alx_book_store;
 
--- Show full description of the books table without using DESCRIBE or EXPLAIN
+-- Show full description of the books table using INFORMATION_SCHEMA
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'alx_book_store'
-AND TABLE_NAME = 'Books';
+AND TABLE_NAME = 'books';
