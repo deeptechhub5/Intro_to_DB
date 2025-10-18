eefb1dd60ffa21f7236f86a34ac6718e5628bb4b
Emmanuel Fiifi Bentsi Essuman <deeptechhub5@gmail.com>
2025-10-18 13:28:46 +0000

    feat: added task_4.sql script

---
+++ task_4.sql
@@ -0,0 +1,8 @@
+-- Select the alx_book_store database
+USE alx_book_store;
+
+-- Show full description of the books table without using DESCRIBE or EXPLAIN
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+AND TABLE_NAME = 'Books';
