1c7c1640fd5cb1a1566ee7284b398872bb671b6a
Emmanuel Fiifi Bentsi Essuman <deeptechhub5@gmail.com>
2025-10-18 14:42:22 +0000

    fix name problem

--- task_6.sql
+++ task_6.sql
@@ -1,12 +1,12 @@
 -- Use the correct database
 USE alx_book_store;
 
--- Insert multiple customer records
+-- Insert customer records (one INSERT per customer, exact spacing)
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES
-(2, 'Blessing Malik', 'bmalik@sandtech.com
-', '124 Happiness Ave.'),
-(3, 'Obed Ehoneah', 'eobed@sandtech.com
-', '125 Happiness Ave.'),
-(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com
-', '126 Happiness Ave.');
+VALUES (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.');
+
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.');
+
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
